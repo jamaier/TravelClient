22aba751319399591910cc89e01b82632c452463
Jacob Maier <jacobamaier@gmail.com>
2023-03-29 11:57:50 -0700

    Update pages
    Co-authored-by: Eliot Gronstal <eliot.lauren@gmail.com>
    Co-Authored-by: Molly Donegan <mollyrdonegan@gmail.com>

--- Controllers/DestinationsController.cs
+++ Controllers/DestinationsController.cs
@@ -1,15 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 using TravelClient.Models;
+using Newtonsoft.Json;
 
 namespace TravelClient.Controllers;
 
 public class DestinationsController : Controller
 {
-  public IActionResult Index()
-  {
-    List<Destination> destinations = Destination.GetDestinations();
+  // [Route("Destinations/Page/{page}")]
+  // public async Task<IActionResult> Index(int page = 1)
+  // {
+  //   HttpClient client = new HttpClient();
+  //   HttpResponseMessage response = await client.GetAsync($"https://localhost:5001/Destinations/page/{page}");
+  //   response.EnsureSuccessStatusCode();
+  //   string responseBody = await response.Content.ReadAsStringAsync();
+  //   DestinationResponse destinationResponse = JsonConvert.DeserializeObject<DestinationResponse>(responseBody);
+
+  //   return View(destinationResponse);
+  // }
+
+  public async Task<IActionResult> Index(int pageNumber = 1, int pageSize = 5)
+{
+    var httpClient = new HttpClient();
+    var response = await httpClient.GetAsync($"https://localhost:5001/api/destinations?page={pageNumber}&pageSize={pageSize}");
+    var content = await response.Content.ReadAsStringAsync();
+    var destinations = JsonConvert.DeserializeObject<List<Destination>>(content);
+
+    ViewBag.PageNumber = pageNumber;
+    ViewBag.PageSize = pageSize;
+    ViewBag.TotalPages = (int)Math.Ceiling((double)destinations.Count / pageSize);
+
     return View(destinations);
-  }
+}
 
   public IActionResult Details(int id)
   {
@@ -39,7 +60,7 @@ public class DestinationsController : Controller
   public ActionResult Edit(Destination destination)
   {
     Destination.Put(destination);
-    return RedirectToAction("Details", new { id = destination.DestinationId});
+    return RedirectToAction("Details", new { id = destination.DestinationId });
   }
 
   public ActionResult Delete(int id)
---
+++ Models/DestinationResponse.cs
@@ -0,0 +1,8 @@
+namespace TravelClient.Models;
+
+    public class DestinationResponse
+    {
+        public List<Destination> Destinations { get; set; } = new List<Destination>();
+        public int Pages { get; set; }
+        public int CurrentPage { get; set; }
+    }
